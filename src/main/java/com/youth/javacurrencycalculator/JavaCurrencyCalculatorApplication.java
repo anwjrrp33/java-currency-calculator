3221c15b71c6b45f92a446bb5c79d7c8397ae1aa
anwjrrp33 <anwjrrp33@gmail.com>
2023-04-29 12:44:58 +0900

    init commit

---
+++ src/main/java/com/youth/javacurrencycalculator/JavaCurrencyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.youth.javacurrencycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCurrencyCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCurrencyCalculatorApplication.class, args);
+    }
+
+}
