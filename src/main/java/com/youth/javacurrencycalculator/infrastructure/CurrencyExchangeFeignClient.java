6842faa7bf355b2b688e5c67aa3a6d59ec1f1ee2
anwjrrp33 <anwjrrp33@gmail.com>
2023-05-02 23:21:23 +0900

    feat: OpenFeign 적용

--- src/main/java/com/youth/javacurrencycalculator/infrastructure/CurrencyExchangeFeignClient.java
+++ src/main/java/com/youth/javacurrencycalculator/infrastructure/CurrencyExchangeFeignClient.java
@@ -1,13 +1,13 @@
 package com.youth.javacurrencycalculator.infrastructure;
 
-import com.youth.javacurrencycalculator.dto.CurrencyResponse;
+import com.youth.javacurrencycalculator.dto.CurrencyExchangeResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "ExchangeRateOpenFeign")
-public interface CurrencyExchangeClient {
+public interface CurrencyExchangeFeignClient {
 
     @GetMapping("/live")
-    CurrencyResponse call(@RequestParam(name = "access_key") String key);
+    CurrencyExchangeResponse call(@RequestParam(name = "access_key") String accessKey);
 }
